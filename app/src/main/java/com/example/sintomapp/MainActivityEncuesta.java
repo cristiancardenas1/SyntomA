6fe358ba54d180fcda997589c7bc892b32482d99
LILIANA GOMEZ <moragomez@une.net.co>
2022-12-16 09:45:09 -0500

    Encuesta síntomas en proceso_V0

---
+++ app/src/main/java/com/example/sintomapp/MainActivityEncuesta.java
@@ -0,0 +1,13 @@
+package com.example.sintomapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityEncuesta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_encuesta);
+    }
+}
